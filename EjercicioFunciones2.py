d3fe63f196810ba5ec8861b40f7462861d0701c9
Antonella Lusardi <69723365+AntoCLus@users.noreply.github.com>
2023-01-18 16:44:25 +0100

    Add files via upload

---
+++ EjercicioFunciones2.py
@@ -0,0 +1,22 @@
+class Main:
+    def __init__(self):
+        self.__class__() 
+    
+def sumita (a, b, c):
+        return a + b + c
+        print(sumita(a, b, c))       
+ 
+class Coche:
+    def __init__(puertas):
+        puertas = puertas
+    def increment(puertas):
+        puertas = 4
+        for i in range(1):
+           print(puertas + 1)
+    
+
+
+print("***Bienvenidos a mi primer ejercicio*****")
+print ("vamos a realizar una suma", sumita(a=10, b=20, c=30))
+miCoche = Coche.__init__(4)
+print("Ahora intentaremos añadirle puertas a Mi coche", miCoche)
