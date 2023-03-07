2e796cf6803e2fa91755e1201f89e2c3430af717
Antonella Lusardi <69723365+AntoCLus@users.noreply.github.com>
2023-03-07 14:36:00 +0100

    Add files via upload

---
+++ FuncionesCreadas.py
@@ -0,0 +1,14 @@
+import  OperacionesBasicas
+
+op1= int(input('ingrese un nro que quiera sumar '))
+op2 = int(input('ingrese un nro que quiera sumar '))
+print("El resultado es:", suma(op1, op2))
+
+op1= int(input('ingrese un nro que quiera restar '))
+op2 = int(input('ingrese un nro que quiera restar '))
+print("El resultado es:", resta(op1, op2))
+
+#esto se puede acortar con un while.
+
+
+
